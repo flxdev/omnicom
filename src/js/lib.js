1692895f79c55fee457e2aa5f917660ba6c45ca0
Tim <andrei.kurlyandchik@gmail.com>
2017-08-30 16:45:51 +0300

    add project omnicom

---
+++ src/js/lib.js
@@ -0,0 +1,34 @@
+//=include lib/jquery.js
+
+//=include lib/barba.js
+
+// //=include lib/jquery.data-parallax.min.js
+// //=include lib/jquery.autosize.js
+
+// //=include lib/TweenLite.min.js
+// //=include lib/CSSPlugin.min.js
+
+//=include lib/stickyfill.min.js
+
+//=include lib/masonry.pkgd.min.js
+
+//=include lib/fm.revealator.jquery.js
+
+//=include lib/jquery.fancybox.min.js
+
+//=include lib/swiper.min.js
+
+// //=include lib/dropzone.js
+
+// //=include lib/viewportchecker.js
+
+// //=include lib/jquery.form-validator.min.js
+
+//=include lib/parallax.min.js
+
+//=include lib/jquery.touchSwipe.min.js
+//=include lib/imagesloaded.min.js
+//=include lib/sangarSlider.js
+//=include lib/sangarSlider2.js
+
+//=include lib/select2.min.js
