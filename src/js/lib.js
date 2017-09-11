f33fdbb5a074ff4ae31d03f892b84105118309ff
Tim <andrei.kurlyandchik@gmail.com>
2017-09-11 10:26:18 +0300

    deleted extra libraries

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,13 +1,5 @@
 //=include lib/jquery.js
 
-//=include lib/barba.js
-
-// //=include lib/jquery.data-parallax.min.js
-// //=include lib/jquery.autosize.js
-
-// //=include lib/TweenLite.min.js
-// //=include lib/CSSPlugin.min.js
-
 //=include lib/stickyfill.min.js
 
 //=include lib/masonry.pkgd.min.js
@@ -17,12 +9,6 @@
 //=include lib/jquery.fancybox.min.js
 
 //=include lib/swiper.min.js
-
-// //=include lib/dropzone.js
-
-// //=include lib/viewportchecker.js
-
-// //=include lib/jquery.form-validator.min.js
 
 //=include lib/parallax.min.js
 
